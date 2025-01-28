661deba460d282aff874febdedca56e65862ed58
Julian <julian.aschwanden@lernende.bbw.ch>
2025-01-28 09:32:17 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Fetch API</h1>
+    <div id="apiDiv">
+        <h2>API Key</h2>
+        <input id="apikey">
+        </div>
+  </div>
+`
