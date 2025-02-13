a04d0ae5ecf0471de2b393080980ffe96649342a
Julian <julian.aschwanden@lernende.bbw.ch>
2025-02-13 11:13:20 +0100

    Add default Option for radio button

--- src/api.tsx
+++ src/api.tsx
@@ -3,7 +3,7 @@ import './style.css'
 
 function Api() {
 
-    const [keyWord, setKeyWord] = useState("");
+    const [keyWord, setKeyWord] = useState("bitcoin");
     const [key, setKey] = useState(localStorage.getItem("keyItem") || " ");
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -41,7 +41,7 @@ function Api() {
                     <fieldset>
                         <legend>Please select your preferred keyword:</legend>
                         <div>
-                            <input type="radio" id="bitcoin" name="keyword" onChange={handleKeywordChange} />
+                            <input type="radio" id="bitcoin" name="keyword" onChange={handleKeywordChange} defaultChecked={true} />
                             <label htmlFor="bitcoin">Bitcoin</label>
 
                             <input type="radio" id="crypto" name="keyword" onChange={handleKeywordChange} />
